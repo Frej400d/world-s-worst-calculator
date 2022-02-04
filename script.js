b225efbe01a8fc0e52dab17ed969790d52088a0f
Freja <frej400d@stud.kea.dk>
2022-02-04 14:28:04 +0100

    calculate

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const calculate = document.querySelector("#calculate");
+let result;
+let operator;
+
+const calculateButton = document.querySelector("#calculate");
+const firstNumber = document.querySelector("#firstnumber");
+const secondNumber = document.querySelector("#secondnumber");
 
 document.addEventListener("DOMContentLoaded", userSetup);
 
 function userSetup() {
-  calculate.addEventListener("click", calculate);
-  console.log("click");
+  calculateButton.addEventListener("click", clickCalculate);
 }
 
-function calculate() {}
+function clickCalculate() {
+  console.log("click calculate");
+  operator = document.querySelector("#operator").value;
+  if (operator === "add") {
+    resultValue = parseInt(firstNumber.value) + parseInt(secondNumber.value);
+  } else if (operator === "sub") {
+    resultValue = firstNumber.value - secondNumber.value;
+  } else if (operator === "mul") {
+    resultValue = firstNumber.value * secondNumber.value;
+  } else if (operator === "div") {
+    resultValue = firstNumber.value / secondNumber.value;
+    console.log("read ");
+  }
+}
