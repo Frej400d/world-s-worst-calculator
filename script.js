4a859c916f9e6027ad91a3d99c52e50d180e3740
Freja <frej400d@stud.kea.dk>
2022-02-04 14:42:38 +0100

    clickcalculate funktion

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+"use strict";
+
 let result;
 let operator;
+let resultValue;
+let rounding;
 
 const calculateButton = document.querySelector("#calculate");
 const firstNumber = document.querySelector("#firstnumber");
 const secondNumber = document.querySelector("#secondnumber");
+const results = document.querySelector("#results");
+const checkbox = document.querySelector("#doround");
 
 document.addEventListener("DOMContentLoaded", userSetup);
 
@@ -12,7 +18,6 @@ function userSetup() {
 }
 
 function clickCalculate() {
-  console.log("click calculate");
   operator = document.querySelector("#operator").value;
   if (operator === "add") {
     resultValue = parseInt(firstNumber.value) + parseInt(secondNumber.value);
@@ -22,6 +27,15 @@ function clickCalculate() {
     resultValue = firstNumber.value * secondNumber.value;
   } else if (operator === "div") {
     resultValue = firstNumber.value / secondNumber.value;
-    console.log("read ");
   }
+  if (checkbox.checked === true) {
+    rounding = parseInt(document.querySelector("#decimals").value);
+    resultValue = resultValue.toFixed(rounding);
+  }
+  firstNumber.value = resultValue;
+
+  result = document.createElement("li");
+  result.textContent = resultValue;
+  results.appendChild(result);
+  results.scrollTop = results.scrollHeight;
 }
